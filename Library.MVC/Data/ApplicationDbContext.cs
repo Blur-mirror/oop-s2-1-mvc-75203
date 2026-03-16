fb96c7dd73be0588c2b1878f8e06deb3ad959190
Blur-mirror <antonioc.adolfo@gmail.com>
2026-03-16 17:08:14 +0000

    Modified DbContext conf, added data seeding logic.

--- Library.MVC/Data/ApplicationDbContext.cs
+++ Library.MVC/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Library.Domain;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,8 +8,26 @@ namespace Library.MVC.Data;
 public class ApplicationDbContext : IdentityDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
+        : base(options) { }
+
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Member> Members { get; set; }
+    public DbSet<Loan> Loans { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
     {
+        base.OnModelCreating(builder);
+
+        builder.Entity<Loan>()
+            .HasOne(l => l.Book)
+            .WithMany(b => b.Loans)
+            .HasForeignKey(l => l.BookId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.Entity<Loan>()
+            .HasOne(l => l.Member)
+            .WithMany(m => m.Loans)
+            .HasForeignKey(l => l.MemberId)
+            .OnDelete(DeleteBehavior.Restrict);
     }
-    public DbSet<Product> Products { get; set; }
 }
